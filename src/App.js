471476ddc4f500a3eb0d85bf06c0a06975a02598
Karen <devkarenc@gmail.com>
2021-04-13 15:16:20 -0400

    Rename Routes.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/">
+          {/* <Home /> */}
+        </Route>
+        <Route exact path="/shop">
+          {/* <Shop /> */}
+        </Route>
+        <Route exact path="/menu">
+          {/* <Menu /> */}
+        </Route>
+        <Route exact path="/cart">
+          {/* <Cart /> */}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
