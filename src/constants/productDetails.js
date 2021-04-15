eec020598265dada09087a84dd67ef9eef584e43
Karen <devkarenc@gmail.com>
2021-04-14 23:36:59 -0400

    Add details for shop products

---
+++ src/constants/productDetails.js
@@ -0,0 +1,93 @@
+import adventureCup from "../images/products/adventure-fuel-cup.jpg";
+import antiqueGrinder from "../images/products/antique-grinder.jpg";
+import bossCup from "../images/products/boss-cup.jpg";
+import chocholateCup from "../images/products/chocolate-cup.jpg";
+import christmasCup from "../images/products/christmas-cup.jpg";
+import coffeeLifeCup from "../images/products/coffee-life-cup.jpg";
+import frenchPress from "../images/products/french-press.jpg";
+import mokaPot from "../images/products/moka-pot.jpg";
+import woodenGrinder from "../images/products/wooden-grinder.jpg";
+
+export const ProductDetails = [
+  {
+    id: 0,
+    imgSrc: adventureCup,
+    imgAlt: "Photo of adventure fuel coffee mug",
+    name: "Adventure Fuel Coffee Mug",
+    price: "14.99",
+    limited: false,
+    onSale: false,
+  },
+  {
+    id: 1,
+    imgSrc: bossCup,
+    imgAlt: "Photo of boss coffee mug",
+    name: "Boss Coffee Mug",
+    price: "12.99",
+    limited: false,
+    onSale: false,
+  },
+  {
+    id: 2,
+    imgSrc: coffeeLifeCup,
+    imgAlt: "Photo of life begins after coffee mug",
+    name: "Life begins after Coffee Mug",
+    price: "13.99",
+    limited: false,
+    onSale: true,
+  },
+  {
+    id: 3,
+    imgSrc: chocholateCup,
+    imgAlt: "Photo of chocolate coffee mug",
+    name: "Chocolate Decor Coffee Mug",
+    price: "15.99",
+    limited: true,
+    onSale: true,
+  },
+  {
+    id: 4,
+    imgSrc: christmasCup,
+    imgAlt: "Photo of christmas coffee mug",
+    name: "Christmas Coffee Mug",
+    price: "19.99",
+    limited: true,
+    onSale: false,
+  },
+  {
+    id: 5,
+    imgSrc: frenchPress,
+    imgAlt: "Photo of french press",
+    name: "Algo French Press",
+    price: "29.99",
+    limited: false,
+    onSale: false,
+  },
+  {
+    id: 6,
+    imgSrc: mokaPot,
+    imgAlt: "Photo of moka pot",
+    name: "Algo Moka Pot",
+    price: "24.99",
+    limited: false,
+    onSale: false,
+  },
+  {
+    id: 7,
+    imgSrc: woodenGrinder,
+    imgAlt: "Photo of wooden grinder",
+    name: "Wooden Coffee Bean Grinder",
+    price: "22.99",
+    limited: false,
+    onSale: true,
+  },
+  {
+    id: 8,
+    imgSrc: antiqueGrinder,
+    imgAlt: "Photo of antique grinder",
+    name: "Antique Coffee Bean Grinder",
+    price: "29.99",
+    limited: true,
+    onSale: false,
+  },
+];
