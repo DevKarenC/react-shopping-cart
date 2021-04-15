e56b87f976bdfe5243d247ca50c8db89462d0ad0
Karen <devkarenc@gmail.com>
2021-04-15 15:08:38 -0400

    Remove unnecessary modal to display product details

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Modal from "react-modal";
 import * as S from "../styled/Shop-styling";
 
 function ProductCard(props) {
@@ -28,15 +27,6 @@ function ProductCard(props) {
     ) : null;
   };
 
-  // modal functions
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const openModal = () => {
-    setIsOpen(true);
-  };
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
   return (
     <div>
       <S.ProductCard>
@@ -44,13 +34,10 @@ function ProductCard(props) {
           <S.ProductImage
             src={props.product.imgSrc}
             alt={props.product.imgAlt}
-            onClick={openModal}
           />
         </div>
         <div className="product-description">
-          <div className="product-name" onClick={openModal}>
-            {props.product.name}
-          </div>
+          <div className="product-name">{props.product.name}</div>
           <div className="product-price">$ {props.product.price}</div>
           <div className="product-specials">
             {isLimited()}
@@ -67,46 +54,6 @@ function ProductCard(props) {
               ADD TO CART
             </button>
           </div>
-          <button onClick={openModal}>VIEW PRODUCT</button>
-          <Modal isOpen={modalIsOpen}>
-            <div>
-              <button className="btn-close-modal" onClick={closeModal}>
-                Close
-              </button>
-            </div>
-            <div className="product-container-modal">
-              <div>
-                <img
-                  src={props.product.imgSrc}
-                  alt={props.product.imgAlt}
-                  className="product-img-modal"
-                />
-              </div>
-              <div>
-                <div className="product-description-modal">
-                  <div className="product-name" onClick={openModal}>
-                    {props.product.name}
-                  </div>
-                  <div className="product-price">$ {props.product.price}</div>
-                  <div className="product-specials">
-                    {isLimited()}
-                    {isOnSale()}
-                  </div>
-                </div>
-                <div className="product-options">
-                  <div className="product-quantity">
-                    <span>Quantity</span>
-                    <input type="number" />
-                  </div>
-                  <div>
-                    <button onClick={onClickAddToCart} className="add-to-cart">
-                      ADD TO CART
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Modal>
         </div>
       </S.ProductCard>
     </div>
